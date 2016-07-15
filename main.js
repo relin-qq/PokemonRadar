560fb065a61387fb1125d399f981f4ef57a2bd9f
relin-qq <a@a.de>
2016-07-15 20:17:44 +0200

    added obscure usage of the pokemon go request message

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ var geo = {
         init: { 
             provider: "google", 
             httpAdapter: "https",
-            formatter : null
         },
         location: "Broadway New York"
     },
@@ -44,63 +43,96 @@ geo.Coder.prototype = {
 //
 
 //Proto
-proto = {}
-proto.PokemonBuf = function(){
+proto = {
+    Req : require("protocol-buffers")
+};
+
+proto.PokemonProtocol = function(){
     var self =  this;
-
-    var protobuf = require("protocol-buffers");
-    var messages = protobuf(Fs.readFileSync("pokemon-go.proto"));
-
-    self = messages;
-
-    // pass a proto file as a buffer/string or pass a parsed protobuf-schema object
-    
-
-    // var buf = messages.Test.encode({
-    //   num: 42,
-    //   payload: 'hello world'
-    // })
-
-    // console.log(buf) // should print a buffer
-    // To decode a message use Test.decode
-
-    // var obj = messages.Test.decode(buf)
-    // console.log(obj) // should print an object similar to above
-    // Enums are accessed in the same way as messages
-
-    // var buf = messages.AnotherOne.encode({
-    //   list: [
-    //     messages.FOO.BAR
-    //   ]
-    // })
-    // Nested emums are accessed as properties on the corresponding message
-
-    // var buf = message.SomeMessage.encode({
-    //   list: [
-    //     messages.SomeMessage.NESTED_ENUM.VALUE
-    //   ]
-    // })
-    // 
-};
-//
-
-var init = function(options){
-    var pokeBuf = new proto.PokemonBuf();
-
-    var geoCoder = new geo.Coder({
-        location: options.location
-    });
-
-    console.log(geoCoder.getLocation(function(res){
-        console.log(res)
-    }));
+    self.messages = proto.Req(Fs.readFileSync("pokemon-go.proto"));
 };
 
+proto.PokemonProtocol.prototype = {
+    //WTF are those values ?
+    createAPIRequest : function(location, token) {
+        var req1 = { type: 2 };
+        var req2 = { type: 126 };
+        var req3 = { type: 4 };
+        var req4 = { type: 129 };
+        var req5 = { 
+            type: 5, 
+            message : { 
+                unknown4: "4a2e9bc330dae60e7b74fc85b98868ab4700802e"
+            } 
+        };
+
+        var request = {
+            unknown1 : 2,
+            rpc_id   : 8145806132888207460,
+            requests : [2, 126, 4, 129],
+            longitude: location.longitude,
+            latitude : location.latitude,
+            altitude : location.altitude,
+            unknown12: 989,
+            requests : [req1, req2, req3, req4, req5],
+            auth: {
+                provider: "ptc",
+                token: {
+                    unknown13: 59,
+                    contents : token
+                }
+            }
+
+        }
+
+        return self.messages.RequestEnvelop.encode(request);
+    },
+
+    login: function(username, password){
+
+    }
+};
+
+var PokemonRadar = {
+    _Http : require("http"),
+    _Https: require("https"),
+
+    Default : {
+        HEADER: { "User-Agent": "Niantic App" },
+        API_URL : "https://pgorelease.nianticlabs.com/plfe/rpc",
+        LOGIN_URL : "https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize",
+        LOGIN_OAUTH : "https://sso.pokemon.com/sso/oauth2.0/accessToken",
+        PTC_CLIENT_SECRET : "w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR"
+    },
+
+    DefaultOptions:{
+        server:{
+            port: 8000,
+            bind: "0.0.0.0"
+        }
+    },
+
+    getApi: function(url, token, req){
+
+    },
+
+    Init : function(options){
+        var pokeProtocol = new proto.PokemonProtocol();
+        var geoCoder = new geo.Coder({location: options.location});
+
+        PokemonRadar._Http.createServer(function (req, res){
+            
+        }).listen(options.port || PokemonRadar.DefaultOptions.server.port,options.bind || PokemonRadar.DefaultOptions.server.bind);
+        
+    }
+}
 
 Program
     .command("start")
     .option("-l --location", 'Sets the location for the radar. Default: 0')
+    .option("-p --port", 'Sets the port for the server. Default: 8080')
+    .option("-b --bind", 'Sets the binding for the server. Default: 0.0.0.0')
     .description("Starts the Pokemon radar")
-    .action(init);
+    .action(PokemonRadar.Init);
 
 Program.parse(process.argv);
